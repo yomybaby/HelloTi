83d47aaf35467ff7b16e02ce7b9eb6763801b92c
Jongeun Lee <yomybaby@yomybaby.com>
2013-12-05 13:30:53 +0900

    model  사용해서 리스트에 보여주기

---
+++ app/controllers/list.js
@@ -0,0 +1,35 @@
+
+var postCol = Alloy.createCollection('post');
+// Alloy.Collection('post').instance();
+
+postCol.on('reset',function(col){
+	var items = [];
+	postCol.each(function(model){
+		items.push({
+			titleLabel : {
+				text : model.get('title')
+			},
+			template : 'postTemplate'
+		});
+	});
+	$.section.setItems(items);
+});
+
+postCol.reset([
+	{
+		title: '제목1',
+		content : '내용1'	
+	},
+	{
+		title: '제목2',
+		content : '내용2'	
+	},
+	{
+		title: '제목3',
+		content : '내용3'	
+	},
+	{
+		title : '제목4'
+	}
+]);
+
