8e7809f405a4db8fe7ebdc280073d262a2f24fe4
Jong Eun Lee <yomybaby@yomybaby.com>
2014-02-19 15:27:17 +0900

    저장 및 취소

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,3 +3,13 @@ $.index.open();
 $.content.addEventListener('change',function(ev){
 	$.length.text = ev.value.length;
 });
+
+$.cancelBtn.addEventListener('click', function(e) {
+	$.content.value = '';
+});
+
+$.saveBtn.addEventListener('click', function(e) {
+	alert($.content.value);
+});
+
+
