2591a1bab7cf7dd52cb44e8dc9bdaf16e38861aa
Jong Eun Lee <yomybaby@yomybaby.com>
2014-04-16 14:35:19 +0900

    사진 첨부

--- app/controllers/postWrite.js
+++ app/controllers/postWrite.js
@@ -1,14 +1,19 @@
 var args = arguments[0] || {};
 
 var postCol = Alloy.Collections.instance('post');
+var selectedPhoto;
 
 $.saveBtn.addEventListener('click', function(e) {
 	postCol.create({
-		content : $.ta.value 
+		content : $.ta.value ,
+		photo : selectedPhoto,
+		"photo_sync_sizes[]" : "original"
 	},{
 		success : function(){
 			$.ta.value = "";
 			$.ta.blur();
+			selectedPhoto = null;
+			
 		},
 		wait : true
 	});
@@ -17,4 +22,25 @@ $.saveBtn.addEventListener('click', function(e) {
 $.cancelBtn.addEventListener('click', function(e) {
 	var toasty = Alloy.createWidget("toasty");
 	toasty.show("Hello World");
+});
+
+
+$.photoBtn.addEventListener('click', function(e) {
+	Titanium.Media.openPhotoGallery({
+		success:function(event)
+		{
+			var image = event.media;
+			
+			selectedPhoto = image;
+		},
+		cancel:function()
+		{
+	
+		},
+		error:function(error)
+		{
+		},
+		allowEditing:true,
+		mediaTypes:[Ti.Media.MEDIA_TYPE_PHOTO]
+	});
 });
